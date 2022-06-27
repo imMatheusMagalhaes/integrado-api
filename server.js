cbcd7f0570f4768e520cfa6004c4da0619d959f6
Matheus <58888259+imMatheusMagalhaes@users.noreply.github.com>
2022-06-27 17:25:07 -0300

    update: update variable name

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-if (process.env.SERVER_PORT !== "production") require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const RoutesService = require("./src/services/RoutesService");
 new RoutesService().init()
